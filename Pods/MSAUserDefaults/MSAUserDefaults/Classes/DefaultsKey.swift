8cde5ad1752fcc09b8dea08f5eb9469f1191cfa3
ilkay Sever <ilkay.sever@pinsoft.ist>
2020-10-21 17:08:30 +0300

    .gitignore remove.

---
+++ Pods/MSAUserDefaults/MSAUserDefaults/Classes/DefaultsKey.swift
@@ -0,0 +1,10 @@
+//
+//  DefaultsKey.swift
+//  MSAUserDefaults
+//
+//  Created by Mehmet Salih Aslan on 6.12.2018.
+//
+
+import Foundation
+
+public class DefaultsKey {}
---
+++ Pods/Target Support Files/MSAUserDefaults/MSAUserDefaults-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MSAUserDefaults : NSObject
+@end
+@implementation PodsDummy_MSAUserDefaults
+@end
